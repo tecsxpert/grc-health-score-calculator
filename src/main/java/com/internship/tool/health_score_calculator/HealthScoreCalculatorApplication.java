2a5f679f260e5857c3548ed3efeaee3d4568355c
yashwanth <yashwanthyashu8111@gmail.com>
2026-04-19 18:21:26 +0530

    Added complete java backend setup

---
+++ src/main/java/com/internship/tool/health_score_calculator/HealthScoreCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.internship.tool.health_score_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthScoreCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthScoreCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internship/tool/health_score_calculator/HealthScoreCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.tool.health_score_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthScoreCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
